3c28a286af93dd843eb7cc0ff3f96303b797bed6
DessDagondon <151791956+DessDagondon@users.noreply.github.com>
2025-05-06 14:09:50 +0800

    Delete myaddons directory

--- myaddons/hotel/models/charges.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-# charges.py
-from odoo import models, fields, api
-class Charges (models.Model):
-     _name = 'hotel.charges'
-     _description = 'Hotel charges master list'
-     name = fields. Char("Charge Name")
-     description = fields.Char("Charge Description")
